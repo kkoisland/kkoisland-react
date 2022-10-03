f2e986d5b1fc40610bba2ae28b5561422166d863
Keiko Higuchi <keiko_higuchi@claris.com>
2022-10-02 21:54:25 -0700

    Create. Add TypeScript, Eslint

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// App.tsx
+function App() {
+
+  return (
+    <div>
+      Welcome!
+    </div>
+  );
+}
+
+ReactDOM.render(
+      <App />,
+  document.getElementById('root')
+);
+
